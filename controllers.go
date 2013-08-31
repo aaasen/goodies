f4a74d1728f8f1b82ab666d5b36d6dde9e7b9004
Lane Aasen <laneaasen@gmail.com>
2013-08-30 17:44:30 -0700

    twilio integration for sending messages now works!

--- controllers.go
+++ controllers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/aaasen/gotwilio"
 	"net/http"
 )
 
@@ -15,6 +16,17 @@ func (c QueryController) Respond(w http.ResponseWriter, r *http.Request, data ma
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 	}
+
+	twilio, err := gotwilio.NewTwilioClientFromFile("twilio-credentials.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	from := "+14254096187"
+	to := "+11234567890"
+	message := string(ddgResponse.GetAnswerText())
+	twilio.SendSMS(from, to, message, "", "")
 
 	fmt.Fprintf(w, string(ddgResponse.GetAnswerText()))
 }
